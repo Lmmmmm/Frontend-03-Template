fda83307f3ed73036b0eab44622e55d1678ecab3
李勐 <lm839377386@gmail.com>
2020-11-03 02:30:03 +0900

    week13

---
+++ week13/jsx/main.js
@@ -0,0 +1,18 @@
+import { Component, createElement } from "./framework.js";
+class Carousel extends Component {
+  //旋转木马，也可以称为轮播图
+  constructor() {
+    this.root = document.createElement("div");
+  }
+}
+
+let d = [
+  "https://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg",
+  "https://static001.geekbang.org/resource/image/1b/21/1b809d9a2bdf3ecc481322d7c9223c21.jpg",
+  "https://static001.geekbang.org/resource/image/b6/4f/b6d65b2f12646a9fd6b8cb2b020d754f.jpg",
+  "https://static001.geekbang.org/resource/image/73/e4/730ea9c393def7975deceb48b3eb6fe4.jpg",
+];
+
+// document.body.appendChild(a);
+let a = <Carousel />;
+a.mountTo(document.body);
---
+++ week13/jsx/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: "./main.js",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: [
+              [
+                "@babel/plugin-transform-react-jsx",
+                { pragma: "createElement" },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  mode: "development", //代表开发者模式，上线的话会改成production
+};
